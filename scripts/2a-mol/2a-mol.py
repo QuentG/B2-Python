4bf1d2a9450b3299d1c6aa6812418d34e49a6289
Quentin Gans <quentin.gans@outlook.fr>
2018-11-05 14:59:30 +0100

    update 2a-mol

--- scripts/2a-mol/2a-mol.py
+++ scripts/2a-mol/2a-mol.py
@@ -1,65 +1,62 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
 
 #Name: 2a-moy
-#Description: Jeu du plus ou moins qui lit dans un fichier.
+#Description: Jeu du plus ou moins qui lis dans un fichier.
 #Date: 23/10/18
 #Gans Quentin
 
 #Importation de modules
 import random
+import re
 import signal
 
-#Fonction qui écrit dans un fichier
+#Fonction qui affiche la solution et au revoir
+def message():
+    write_in_file('A la prochaine ! - La solution était', str(nbr))
+    exit()
+    
+ #Fonction qui écrit dans un fichier
 def write_in_file(msg):
-  file = open(path_file, "w")
+  file = open("plusoumoins.txt", "w")
   file.write(msg)
   file.close()
-
+  
 #Fonction qui lis dans du fichier
 def read_in_file():
-  file = open(path_file, "r")
-  input = file.readline().strip()
+  file = open("plusoumoins.txt", "r")
+  msg = file.readline().strip()
   file.close()
-  return input
-
-#Fonction qui check si c'est un nombre
-def checkNumber():
-  number = read_in_file()
-  #Si c'est pas un nombre on retry
-  while(number.isdigit() == False):
-    number = read_in_file()
-  return int(number)
-
-#Fonction jeu qui va retourner trop grand, trop petit ou gagné
-def game():
-  if (nbr == saisi):
-    end = True
-    return 'gagné'
-  elif (saisi > nbr):
-    return 'trop grand'
-  else:
-    return 'trop petit'
+  return msg
 
 #Fonction qui quitte le prog si on CTRL+C
 def end_game(sig, frame):
-    write_in_file('\nPas ouf de CTRL+C ')
+    write_in_file('Pas ouf de CTRL+C ')
     exit()
-
 #Si il CTRL+C
 signal.signal(signal.SIGINT, end_game)
 
-#Variables
-path_file = "plusoumoins.txt"
+ #Variables
 end = False
 nbr = random.randint(0,100)
-saisi = -1
 
-write_in_file('Entrez un nombre entre 0 et 100 ! : ')
+write_in_file('Bonjour, entrez un nombre entre 0 et 100 ! : ')
 
 #Logique du jeu
-while (saisi != nbr and end == False):
-    saisi = checkNumber()
-    result = game()
-    write_in_file(result)
-
-        
+while end is False :
+    #On va lire dans le file 
+    saisi = read_in_file()
+     #On check que ce soit un int 
+    if re.match("^[0-9]+$", saisi):
+        saisi = int(saisi)
+        #Et qu'il ne soit pas au dessus de 100
+        if saisi > 100:
+            continue          
+        if saisi > nbr :
+            write_in_file('Trop grand !')       
+        elif saisi < nbr :
+            write_in_file('Trop petit !')
+        #Sinon on affiche win, on set le end en win
+        else :
+            write_in_file('Gg a toi ! ')
+            end = True
+            message()
